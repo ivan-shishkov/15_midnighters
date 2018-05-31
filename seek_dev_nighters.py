8e16ef9b955050ce67ac68bae94c984659b0f7c3
Ivan Shishkov <mail-shishkov@yandex.ru>
2018-05-31 20:22:00 +0300

    Added function of executing of the GET request

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,3 +1,16 @@
+import requests
+from requests.exceptions import ConnectionError
+
+
+def execute_get_request(url, params=None):
+    try:
+        response = requests.get(url, params=params)
+    except ConnectionError:
+        return None
+
+    return response.json() if response.ok else None
+
+
 def main():
     pass
 
