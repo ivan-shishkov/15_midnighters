d20968cb2bd8c79aa453f6b40369390c686b442b
Ivan Shishkov <mail-shishkov@yandex.ru>
2018-05-31 22:12:42 +0300

    Added getting of info about solution attempts using Devman API

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -11,8 +11,31 @@ def execute_get_request(url, params=None):
     return response.json() if response.ok else None
 
 
+def get_solution_attempts_info():
+    solution_attempts_info = []
+
+    page_number = 1
+
+    while True:
+        solution_attempts_info_page = execute_get_request(
+            url='https://devman.org/api/challenges/solution_attempts',
+            params={
+                'page': page_number,
+            },
+        )
+        if solution_attempts_info_page is None:
+            return None
+
+        solution_attempts_info.extend(solution_attempts_info_page['records'])
+
+        if page_number >= solution_attempts_info_page['number_of_pages']:
+            return solution_attempts_info
+
+        page_number = page_number + 1
+
+
 def main():
-    pass
+    solution_attempts_info = get_solution_attempts_info()
 
 
 if __name__ == '__main__':
